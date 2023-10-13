5eb6ac67ace91eec53fa6ebe656666fb09575f6a
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-13 17:35:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import Search from "./components/Search"
 import UserInfo from "./components/UserInfo"
 
 export default function App() {
   return (
     <main className="mx-auto w-[85%] my-8 grid gap-6">
-      <Search />
       <UserInfo />
     </main>
   )  
