eb021dd91cb10514f777762c3c473968f7468b11
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-13 17:35:53 -0400

    Update UserInfo.jsx

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,40 +1,79 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faLocationDot, faTag, faUserCheck, faUserPlus, faStar, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLocationDot, faTag, faUserCheck, faUserPlus, faStar, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from 'react';
+import Search from './Search';
 
-const spaceIcons = 'flex gap-2 items-center justify-start'
+const spaceIcons = 'flex gap-2 items-center justify-start';
 
 export default function UserInfo() {
-    return (
+  const [username, setUsername] = useState("mauriciompf") 
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const handleSearch = async (seachUsername) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`https://api.github.com/users/${seachUsername}`);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+      }
+
+      const userData = await response.json();
+      setData(userData);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    handleSearch(username);
+  }, [username]);
+
+  const renderIconItem = (icon, value) => (
+    <li className={spaceIcons}>
+        <FontAwesomeIcon icon={icon} />
+        <span>{value}</span>
+    </li>
+  )
+
+  return (
+    <>
+      <Search onSearch={handleSearch} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <section className='rounded-lg gap-4 grid bg-white p-3 '>
-            <div className='relative'>
-                <article className='grid gap-2'>
-                    <div className='grid gap-4'>
-                        <div className='rounded-lg bg-yellow-600 w-24 aspect-square '>
-                                <img src={item.avatar_url}  alt={item.login} />
-                        </div>
-                        <p className='grid'>
-                            <span className='font-bold'>{item.name}</span>
-                            <span>{item.login}</span>
-                        </p>
-                        <p>
-                            {item.bio}
-                        </p>
-                    </div>
-                </article>
-                <ul className='absolute top-0 right-0 grid gap-2'>
-                    <li className={spaceIcons}><FontAwesomeIcon icon={faLocationDot} /><span>{item.location}</span></li>
-                    <li className={spaceIcons}><FontAwesomeIcon icon={faTwitter} /><span>{item.twitter_username}</span></li>
-                    <li className={spaceIcons}><FontAwesomeIcon icon={faUserCheck} /><span>item.following</span></li>
-                    <li className={spaceIcons}><FontAwesomeIcon icon={faUserPlus} /><span>item.followers</span></li>
-                </ul>
-            </div>
-            <ul className='grid gap-2'>
-                <li className={spaceIcons}><FontAwesomeIcon icon={faStar} />{item.public_gists}</li>
-                <li className={spaceIcons}><FontAwesomeIcon icon={faGlobe} />{item.site_admin}</li>
-                <li className={spaceIcons}><FontAwesomeIcon icon={faGithub} />{item.url}</li>
-                <li className={spaceIcons}><FontAwesomeIcon icon={faTag} />{item.type}</li>
+          <div className='relative'>
+            <article className='grid gap-2'>
+              <div className='grid gap-4'>
+                <div className='w-24 aspect-square'>
+                  <img className='rounded-lg' src={data.avatar_url} alt={data.login} />
+                </div>
+                <p className='grid'>
+                  <span className='font-bold'>{data.name}</span>
+                  <span>{data.login}</span>
+                </p>
+                <p>{data.bio}</p>
+              </div>
+            </article>
+            <ul className='absolute top-0 right-0 grid gap-2'>
+                {renderIconItem(faLocationDot, data.location)}
+                {data.twitter_username !== null && renderIconItem(faTwitter, data.twitter_username)}
+                {renderIconItem(faUserCheck, data.following)}
+                {renderIconItem(faUserPlus, data.followers)}
             </ul>
-        </section>
-    )   
+          </div>
+          <ul className='grid gap-2'>
+            {renderIconItem(faStar, data.public_gists)}
+            {renderIconItem(faGlobe, data.site_admin ? 'Admin' : 'User')}
+            {renderIconItem(faGithub, data.html_url)}
+            {renderIconItem(faTag, data.type)}
+          </ul>
+          </section>
+      )}
+    </>
+  );
 }
