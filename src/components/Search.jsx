cf6853a9c5f0bcc5e29d3fe4367d261cd5c326f0
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-14 17:56:09 -0400

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,10 +8,10 @@ export default function Search({ onSearch }) {
     }
 
     return (
-        <section className="rounded-lg bg-white p-3 flex justify-between gap-4">
+        <section className="rounded-lg bg-white p-3 flex gap-4">
             <span className="sr-only">Search Bar</span>
-            <input className="p-6 bg-slate-200 w-[min(70%,_200px)] font-semibold rounded-lg " type="search" placeholder="Search for a GitHub user" value={username} onChange={(e) => setUsername(e.target.value)}/>
-            <button className="bg-green-500 rounded-lg font-semibold" onClick={handleSearch}>Search Github</button>
+            <input className="p-6 bg-slate-200 w-full font-semibold rounded-lg hover:bg-slate-100 focus:bg-slate-100" type="search" placeholder="Search for a GitHub user" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <button className="bg-green-400 rounded-lg font-semibold hover:bg-green-300 focus:bg-green-300 transition-colors" onClick={handleSearch}>Search Github</button>
         </section>
     )
 }
